5b912f4a3ca6ce6a3fad6b1fbdf62bb610bf4ad2
twizy <twizalain@gmail.com>
2021-01-19 11:08:07 +0200

    Adding home page

---
+++ hotel-vue/src/pages/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>All hotels</h1>
+
+            <app-hotel-card></app-hotel-card>
+
+    </div>
+</template>
+
+<script>
+
+import hotelcard from '../components/hotelcard.vue';
+
+export default {
+    components:{
+        'app-hotel-card':hotelcard,
+    },
+    
+    
+}
+</script>
+
+<style scoped>
+
+</style>
