e03543677fffb1f50a7e40f20e9098ded9127372
twizy <twizalain@gmail.com>
2021-01-19 12:18:43 +0200

    some changes

--- hotel-vue/src/App.vue
+++ hotel-vue/src/App.vue
@@ -57,4 +57,7 @@ li {
 a {
   color: #42b983;
 }
+/* @Github.com.bi */
 </style>
+
+
--- hotel-vue/src/components/forms/hotelform.vue
+++ hotel-vue/src/components/forms/hotelform.vue
@@ -1,12 +1,14 @@
 <template>
 
     <div class="mb-3 mx-2">
-        <form action="">
+        <form action="" enctype="multipart/form-data">
             <input type="text" class="form-control" v-model="$store.state.quaState.hotel" placeholder="Hotel name"> <br>
             
             <select class="form-select" aria-label="Default select example" v-model="$store.state.quaState.province">
                 <option :key="i.id" v-for="i in $store.state.allProvinces">{{i.province}}</option>
             </select> <br>
+
+
 
             <input class="form-control form-control-md" id="formFileSm" type="file"> <br>
            
@@ -21,7 +23,9 @@
 
             <input class="form-control form-control-md" id="formFileSm" type="file"> <br>
           
-            <input class="form-control form-control-md" id="formFileSm" type="file"> <br>
+            <input class="form-control form-control-md" id="formFileSm" type="file" @change="onfileselect()"> <br>
+
+           
             
             <button class="btn btn-outline-primary" @click.prevent="addHotel">Add hotel</button>
         </form>
@@ -34,28 +38,49 @@
 import axios from 'axios'
 
 export default {
+    data(){
+        return{
+            selectfile:"",
+        }
+    },
     methods:{
         addHotel(){
-            const myForm={
-                hotel:this.$store.state.quaState.hotel,
-                province:this.$store.state.quaState.province,
-                photo_outside:null,
-                photo_inside:null,
-                photo_room:null,
-                date:'2021-01-09'
-            }
+            var datas = new FormData()
 
-            var checkExitingObject = this.$store.state.allHotels.filter(function (elm){
-                if (elm.hotel.toLowerCase() == myForm.hotel.toLowerCase() && 
-                elm.province.toLowerCase() == myForm.province.toLowerCase())
-                {
-                    return elm; // returns length = 1 (object exists in array)
-                }
-            });
+            datas.append('hotel', this.$store.state.quaState.hotel)
+            datas.append('province', this.$store.state.quaState.province)
+            datas.append('photo_outside', null)
+            datas.append('photo_inside', null)
+            datas.append('photo_room', this.selectfile) //it means this.selectfile will have event.target.files[0].
+            datas.append('date', '2021-01-09')
 
-            if(checkExitingObject.length == 0){
+            console.log(datas)
+            // const myForm={
+            //     hotel:this.$store.state.quaState.hotel,
+            //     province:this.$store.state.quaState.province,
+            //     photo_outside:null,
+            //     photo_inside:null,
+            //     photo_room:this.selectfile,
+            //     date:'2021-01-09'
+            // }
 
-            axios.post('http://localhost:8000/api/hotel/hotel-create/',myForm)
+            // console.log(myForm)
+
+            // var checkExitingObject = this.$store.state.allHotels.filter(function (elm){
+            //     if (elm.hotel.toLowerCase() == myForm.hotel.toLowerCase() && 
+            //     elm.province.toLowerCase() == myForm.province.toLowerCase())
+            //     {
+            //         return elm; // returns length = 1 (object exists in array)
+            //     }
+            // });
+
+            // if(checkExitingObject.length == 0){
+
+
+
+
+
+            axios.post('http://localhost:8000/api/hotel/hotel-create/',datas)
                 .then(res=>{
                     this.getHotel()
                 })
@@ -68,13 +93,18 @@ export default {
                     this.$store.state.quaState.photo_room = "",
                     this.$store.state.quaState.date = "",)
 
-            }
-            else if(checkExitingObject.length == 1){
-                console.log("Hotel exists")
-            }
-            else{
-               console.log("Sorry put valid input") 
-            }
+
+
+
+
+
+            // }
+            // else if(checkExitingObject.length == 1){
+            //     console.log("Hotel exists")
+            // }
+            // else{
+            //    console.log("Sorry put valid input") 
+            // }
 
         },
         getHotel(){
@@ -94,6 +124,9 @@ export default {
                 console.log(error)
             })
         },
+        onfileselect(){
+            this.selectfile = event.target.files[0];
+        }
     },
     created(){
         this.getHotel()
